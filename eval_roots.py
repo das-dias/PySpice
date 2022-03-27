aa16808839130443397ed6a8b5c859d9e5bc7126
Roman Parise <roman@lemonsolutions.info>
2022-03-27 15:48:27 -0700

    Remove a lot of unnecessary OpenSPICE files

--- eval_roots.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import scipy.optimize
-
-def roots_from_txt(behav_txt):
-    return scipy.optimize.root(eval("lambda x : " + behav_txt), [0.0], method = 'lm').x[0]
-
-if __name__ == "__main__":
-    print(roots_from_txt("(x[0] ** 2.0) - 1"))
--- profile.sh
+++
@@ -1,1 +0,0 @@
-python3 -m cProfile -s cumtime ./OpenSPICE.py circuit5_trans_os.cir > log
