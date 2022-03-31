4f45a41fea272ed5a9ecad8126869ea53a3bd058
Roman Parise <roman@lemonsolutions.info>
2022-03-31 12:36:01 -0700

    Added capacitor support. No test yet.

--- OpenSPICE-revamp/parser.py
+++ OpenSPICE-revamp/parser.py
@@ -7,16 +7,28 @@ from arpeggio import ZeroOrMore, EOF, Optional, OneOrMore, RegExMatch, ParserPyt
 class Resistor:
     pass
 
+class Capacitor:
+    pass
+
 #######################################################################################
 
 def netlist():
     return ZeroOrMore(branch, OneOrMore(newline)), Optional(branch)
 
 def branch():
-    return resistor
+    return [resistor, capacitor]
+
+def capacitor():
+    return ccomponent, node, node, passiveValue, Optional(ic)
 
 def resistor():
     return rcomponent, node, node, passiveValue
+
+def ic():
+    return RegExMatch(r'ic='), stateVarValue
+
+def ccomponent():
+    return RegExMatch(r'C\d+')
 
 def rcomponent():
     return RegExMatch(r'R\d+')
@@ -25,6 +37,9 @@ def node():
     return RegExMatch(r'\d+')
 
 def passiveValue():
+    return RegExMatch(r'\d+')
+
+def stateVarValue():
     return RegExMatch(r'\d+')
 
 def newline():
@@ -41,12 +56,21 @@ def nonterm_is_branch(nonterm):
 
 def gen_dict_from_branch(nonterm):
     assert nonterm_is_branch(nonterm)
-    if nonterm[0].rule_name == "rcomponent":
-        assert len(nonterm) == 4
+    if   nonterm[0].rule_name == "resistor":
+        assert len(nonterm[0]) == 4
         return {"component"  : Resistor,
-                "node_plus"  : nonterm[1].value,
-                "node_minus" : nonterm[2].value,
-                "value"      : nonterm[3].value}
+                "node_plus"  : nonterm[0][1].value,
+                "node_minus" : nonterm[0][2].value,
+                "value"      : nonterm[0][3].value}
+    elif nonterm[0].rule_name == "capacitor":
+        assert len(nonterm[0]) == 4 or len(nonterm[0]) == 5
+        _cap = {"component"  : Capacitor,
+                "node_plus"  : nonterm[0][1].value,
+                "node_minus" : nonterm[0][2].value,
+                "value"      : nonterm[0][3].value}
+        if len(nonterm[0]) == 5:
+            _cap["ic" : nonterm[0][4].value.replace("ic=", "")]
+        return _cap
     else:
         assert False
 
