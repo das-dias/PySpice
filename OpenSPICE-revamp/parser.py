34aadeadf7b5f637b3950a02b9c35627b19b3e3e
Roman Parise <roman@lemonsolutions.info>
2022-03-31 12:27:24 -0700

    Cleaned up resistor parsing

--- OpenSPICE-revamp/parser.py
+++ OpenSPICE-revamp/parser.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
-from arpeggio import ZeroOrMore, EOF, Optional, OneOrMore, RegExMatch, ParserPython
+from arpeggio import ZeroOrMore, EOF, Optional, OneOrMore, RegExMatch, ParserPython, Terminal, NonTerminal
+
+#######################################################################################
+
+class Resistor:
+    pass
+
+#######################################################################################
 
 def netlist():
     return ZeroOrMore(branch, OneOrMore(newline)), Optional(branch)
@@ -23,7 +30,38 @@ def passiveValue():
 def newline():
     return RegExMatch(r'\n')
 
+#######################################################################################
+
+def filter_terms(ptree):
+    return [_ for _ in ptree if type(_) != Terminal]
+
+def nonterm_is_branch(nonterm):
+    assert type(nonterm) == NonTerminal
+    return "branch" in nonterm.name
+
+def gen_dict_from_branch(nonterm):
+    assert nonterm_is_branch(nonterm)
+    if nonterm[0].rule_name == "rcomponent":
+        assert len(nonterm) == 4
+        return {"component"  : Resistor,
+                "node_plus"  : nonterm[1].value,
+                "node_minus" : nonterm[2].value,
+                "value"      : nonterm[3].value}
+    else:
+        assert False
+
+def gen_dict(nonterm):
+    if nonterm_is_branch(nonterm):
+        return gen_dict_from_branch(nonterm)
+    else:
+        assert False
+
+def gen_data_dicts(ptree):
+    return [gen_dict(_) for _ in ptree]
+
 if __name__ == "__main__":
     parser = ParserPython(netlist, ws='\t\r ')
     with open("multi_r_netlist.cir") as f:
-        print(parser.parse(f.read()).tree_str())
+        ptree = parser.parse(f.read()) 
+        ptree = filter_terms(ptree)
+        print(gen_data_dicts(ptree))
