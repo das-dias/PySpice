c1978c8b1703035a4cb0b6d10174c020f2c13c87
Roman Parise <roman@lemonsolutions.info>
2022-03-31 20:28:02 -0700

    Modify common formatting to support most of the behavioral source syntax

--- OpenSPICE-revamp/common.py
+++ OpenSPICE-revamp/common.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def v_format(s):
-    return "x['v({})']".format(s)
+    return "v({})".format(s)
 
 def i_format(s):
-    return "x['i({})']".format(s)
+    return "i({})".format(s)
