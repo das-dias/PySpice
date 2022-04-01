145c26316f0972efb4798b95e9188292557f2f92
Roman Parise <roman@lemonsolutions.info>
2022-03-31 18:05:21 -0700

    Add strategy pattern skeleton to eqnstr.py

--- OpenSPICE-revamp/eqnstr.py
+++ OpenSPICE-revamp/eqnstr.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
 
 from components import Resistor, Capacitor, Inductor, VSource, ISource
+from abc import ABC, abstractmethod
+
+class EqnStrContext:
+    def __init__(self, strategy):
+        self.strategy = strategy
+    def execute_strategy(self, parser_output_dict):
+        return self.strategy.do_operation(parser_output_dict)
+
+class EqnStrStrategy(ABC):
+    @abstractmethod
+    def do_operation(self, parser_output_dict):
+        pass
