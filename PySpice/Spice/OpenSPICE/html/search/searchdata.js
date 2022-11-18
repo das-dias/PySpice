97384bc2123a21c61fa674243a2db9e81310bdd0
Roman Parise <roman@lemonsolutions.info>
2022-11-18 15:22:45 -0800

    Add documentation

---
+++ PySpice/Spice/OpenSPICE/html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "_abcdefghilnoprstuvw",
+  1: "bceioprstv",
+  2: "_adefghlnoprstuw"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Functions"
+};
+
